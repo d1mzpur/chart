78ddc879b36fc4fe935285e33cc84f9651ec5e8a
Dimas Purnomo <dimzpurnomo@gmail.com>
2022-08-03 11:11:38 +0700

    init chart

---
+++ lib/main.dart
@@ -0,0 +1,282 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_echarts/flutter_echarts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, Object>> data = [
+    {
+      "name": "HM1",
+      "itemStyle": {
+        "color": '#00B04F',
+      },
+    },
+    {
+      "name": "HM2",
+      "itemStyle": {
+        "color": '#00B04F',
+      },
+    },
+    {
+      "name": "HM3",
+      "itemStyle": {
+        "color": '#00B04F',
+      },
+    },
+    {
+      "name": "MT1",
+      "itemStyle": {
+        "color": '#00B04F',
+      },
+    },
+    {
+      "name": "H1",
+      "itemStyle": {
+        "color": '#00B04F',
+      },
+    },
+    {
+      "name": "Storage",
+      "itemStyle": {
+        "color": '#3F3F3F',
+      }
+    },
+    {
+      "name": "ExportHarvest",
+      "itemStyle": {
+        "color": '#000000',
+      }
+    },
+    {
+      "name": "StoreHarvest",
+      "itemStyle": {
+        "color": '#01B0F2',
+      }
+    },
+    {
+      "name": "Enjoy",
+      "itemStyle": {
+        "color": '#FE0000',
+      }
+    },
+    {
+      "name": "Aircon",
+      "itemStyle": {
+        "color": '#FE0000',
+      }
+    },
+    {
+      "name": "Utility",
+      "itemStyle": {
+        "color": '#FE0000',
+      }
+    },
+    {
+      "name": "Light",
+      "itemStyle": {
+        "color": '#FE0000',
+      }
+    },
+    {
+      "name": "Other",
+      "itemStyle": {
+        "color": '#FE0000',
+      }
+    },
+    {
+      "name": "EnjoyStore",
+      "itemStyle": {
+        "color": '#000000',
+      }
+    },
+    {
+      "name": "ExportStore",
+      "itemStyle": {
+        "color": '#01B0F1',
+      }
+    },
+  ];
+
+  List<Map<String, Object>> links = [
+    {
+      "source": "HM1",
+      "target": "MT1",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "HM2",
+      "target": "MT1",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "HM3",
+      "target": "MT1",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "MT1",
+      "target": "H1",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "H1",
+      "target": "Storage",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "ExportHarvest",
+      "target": "Storage",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "StoreHarvest",
+      "target": "Storage",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Storage",
+      "target": "Enjoy",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Enjoy",
+      "target": "Aircon",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Enjoy",
+      "target": "Utility",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Enjoy",
+      "target": "Light",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Enjoy",
+      "target": "Other",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Storage",
+      "target": "EnjoyStore",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+    {
+      "source": "Storage",
+      "target": "ExportStore",
+      "value": 1,
+      "lineStyle": {
+        "color": 'gradient',
+      }
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey =
+        new GlobalKey<ScaffoldState>();
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Echart'),
+      ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          child: Echarts(
+            option: '''
+              {
+                tooltip: {
+                  trigger: 'item',
+                  triggerOn: 'click'
+                },
+                series: {
+                  type: 'sankey',
+                  layout: 'none',
+                  emphasis: {
+                    focus: 'adjacency'
+                  },
+                data:  ${jsonEncode(data)},
+                links: ${jsonEncode(links)},
+                }
+              }
+                ''',
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        ),
+      ),
+    );
+  }
+}
