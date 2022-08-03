05a7de30754e09531038dcadd47c311189d84b84
Dimas Purnomo <dimzpurnomo@gmail.com>
2022-08-03 13:13:06 +0700

    testing new data

--- lib/main.dart
+++ lib/main.dart
@@ -235,6 +235,90 @@ class _MyHomePageState extends State<MyHomePage> {
     },
   ];
 
+  getData1() async {
+    await Future.delayed(Duration(seconds: 4));
+
+    const dataObj = [
+      {
+        "name": "H",
+        "itemStyle": {
+          "color": '#00B04F',
+        },
+      },
+      {
+        "name": "Storage",
+        "itemStyle": {
+          "color": '#3F3F3F',
+        }
+      },
+      {
+        "name": "ExportHarvest",
+        "itemStyle": {
+          "color": '#000000',
+        }
+      },
+      {
+        "name": "StoreHarvest",
+        "itemStyle": {
+          "color": '#01B0F2',
+        }
+      },
+      {
+        "name": "Enjoy",
+        "itemStyle": {
+          "color": '#FE0000',
+        }
+      }
+    ];
+
+    this.setState(() {
+      this.data = dataObj;
+    });
+  }
+
+  getData2() async {
+    await Future.delayed(Duration(seconds: 4));
+
+    const dataObj = [
+      {
+        "source": "H",
+        "target": "Storage",
+        "value": 3,
+        "lineStyle": {
+          "color": 'gradient',
+        }
+      },
+      {
+        "source": "ExportHarvest",
+        "target": "Storage",
+        "value": 1,
+        "lineStyle": {
+          "color": 'gradient',
+        }
+      },
+      {
+        "source": "StoreHarvest",
+        "target": "Storage",
+        "value": 1,
+        "lineStyle": {
+          "color": 'gradient',
+        }
+      },
+      {
+        "source": "Storage",
+        "target": "Enjoy",
+        "value": 1,
+        "lineStyle": {
+          "color": 'gradient',
+        }
+      }
+    ];
+
+    this.setState(() {
+      this.links = dataObj;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -253,28 +337,41 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       backgroundColor: Colors.white,
       body: Center(
-        child: Container(
-          child: Echarts(
-            option: '''
-              {
-                tooltip: {
-                  trigger: 'item',
-                  triggerOn: 'click'
+        child: SingleChildScrollView (
+          child: Column(
+            children: [
+              Container(
+                child: Echarts(
+                  option: '''
+                    {
+                      tooltip: {
+                        trigger: 'item',
+                        triggerOn: 'click'
+                      },
+                      series: {
+                        type: 'sankey',
+                        layout: 'none',
+                        emphasis: {
+                          focus: 'adjacency'
+                        },
+                      data:  ${jsonEncode(data)},
+                      links: ${jsonEncode(links)},
+                      }
+                    }
+                      ''',
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  getData1();
+                  getData2();
                 },
-                series: {
-                  type: 'sankey',
-                  layout: 'none',
-                  emphasis: {
-                    focus: 'adjacency'
-                  },
-                data:  ${jsonEncode(data)},
-                links: ${jsonEncode(links)},
-                }
-              }
-                ''',
+                child: Text("Ganti Data"),
+              ),
+            ],
           ),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
         ),
       ),
     );
